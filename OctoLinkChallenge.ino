8822a192950861d35ede4b08ad721cf5c0308a58
Edgar W <techniccontroller@gmail.com>
2025-07-06 17:10:01 +0200

    add dfplayer code

--- OctoLinkChallenge.ino
+++ OctoLinkChallenge.ino
@@ -1,10 +1,28 @@
 #include <Adafruit_NeoPixel.h>
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
 
 #define SPEAKER_RX 10 // connects to DFPLayer TX pin
 #define SPEAKER_TX 11 // connects to DFPLayer RX pin
 
 #define NEOPIXEL_PIN 12 // Pin for NeoPixel data line
 #define NUMPIXELS 8 // Number of NeoPixels
+
+
+#define CHECK_CONNECTION_INTERVAL 100 // Interval to check connections in milliseconds
+
+
+// Define different mp3 files
+#define MP3_FILE_FANFARE 1    // on SD card, file name: /mp3/0001.mp3
+#define MP3_FILE_FAILURE 2    // on SD card, file name: /mp3/0002.mp3
+#define MP3_FILE_GOODRESULT 3 // on SD card, file name: /mp3/0003.mp3
+#define MP3_FILE_SUCCESS 4    // on SD card, file name: /mp3/0004.mp3
+#define MP3_FILE_WINNING 5    // on SD card, file name: /mp3/0005.mp3
+#define MP3_FILE_ELEVATION 6  // on SD card, file name: /mp3/0006.mp3
+#define MP3_FILE_MAJESTIC 7   // on SD card, file name: /mp3/0007.mp3
+#define MP3_FILE_VICTORY 8    // on SD card, file name: /mp3/0008.mp3
+
+
 
 const int connectionsInputPins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 const int connectionsOutputPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
@@ -14,11 +32,59 @@ uint8_t connectionStates[numberOfConnections] = {0};
 uint8_t previousConnectionStates[numberOfConnections] = {0};
 
 unsigned long previousMillis = 0;
-const long interval = 100; // Interval to check pins (100 milliseconds)
 // Animation frame interval (adjust as needed for smoothness)
 const unsigned long frameInterval = 100;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+SoftwareSerial softSerial(/*rx =*/SPEAKER_RX, /*tx =*/SPEAKER_TX);
+#define FPSerial softSerial
+DFRobotDFPlayerMini myDFPlayer;
+
+bool dfPlayerInitialized = false;
+
+uint8_t initDFPlayer() {
+
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  const int maxRetries = 5;
+  int attempts = 0;
+
+  while (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {
+    attempts++;
+    Serial.print(F("Attempt "));
+    Serial.print(attempts);
+    Serial.println(F(" failed to start DFPlayer."));
+
+    if (attempts >= maxRetries) {
+      Serial.println(F("Unable to begin:"));
+      Serial.println(F("1. Please recheck the connection!"));
+      Serial.println(F("2. Please insert the SD card!"));      
+      dfPlayerInitialized = false;
+      return 1; // Return 1 to indicate failure
+    }
+
+    delay(1000); // Wait 1 second before retrying
+  }
+
+  Serial.println(F("DFPlayer Mini online."));
+  dfPlayerInitialized = true;
+  return 0; // Return 0 to indicate success
+}
+
+void showLEDSingleColor(uint32_t color, int duration){
+  // Set all NeoPixels to the specified color
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, color);
+  }
+  pixels.show(); // Update the NeoPixels
+
+  delay(duration); // Keep the color for the specified duration
+
+  // Clear the NeoPixels
+  pixels.clear();
+  pixels.show(); // Update the NeoPixels
+}
 
 uint8_t getConnectionState(int index) {
   // write output pin to LOW, then read input pin, if the input pin is LOW, the connection is closed
@@ -29,28 +95,40 @@ uint8_t getConnectionState(int index) {
   return state == 0 ? 1 : 0; // Return 1 if closed, 0 if open
 }
 
+void printConnectionStates() {
+  Serial.print("connection states: [");
+  for (int i = 0; i < numberOfConnections; i++) {
+    Serial.print(connectionStates[i]);
+    if (i < numberOfConnections - 1) {
+      Serial.print(", ");
+    }
+  }
+  Serial.println("]");
+}
+
+void playFinishedMelody() {
+  //myDFPlayer.playMp3Folder(MP3_FILE_WINNING);
+  //myDFPlayer.playMp3Folder(MP3_FILE_FANFARE);
+  //myDFPlayer.playMp3Folder(MP3_FILE_MAJESTIC);
+  myDFPlayer.playMp3Folder(MP3_FILE_VICTORY);
+}
+
+void playConnectionClosedMelody() {
+  myDFPlayer.playMp3Folder(MP3_FILE_SUCCESS);
+}
+
+void playConnectionOpenedMelody() {
+  myDFPlayer.playMp3Folder(MP3_FILE_FAILURE);
+}
+
 // This function can be used to run an LED event when a connection is closed
 void runLEDEventConnectionClosed(){
-  // For example, you can light up the NeoPixels in a specific color
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Set to green color
-  }
-  pixels.show(); // Update the NeoPixels
-  delay(500); // Keep the color for 500 milliseconds
-  pixels.clear(); // Clear the NeoPixels
-  pixels.show(); // Update the NeoPixels
+  showLEDSingleColor(pixels.Color(0, 255, 0), 500); // Show green color for 500 milliseconds
 }
 
 // This function can be used to run an LED event when a connection is opened
 void runLEDEventConnectionOpened(){
-  // For example, you can light up the NeoPixels in a different color
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(255, 0, 0)); // Set to red color
-  }
-  pixels.show(); // Update the NeoPixels
-  delay(500); // Keep the color for 500 milliseconds
-  pixels.clear(); // Clear the NeoPixels
-  pixels.show(); // Update the NeoPixels
+  showLEDSingleColor(pixels.Color(255, 0, 0), 500); // Show red color for 500 milliseconds
 }
 
 void runLEDEventFinished() {
@@ -60,22 +138,31 @@ void runLEDEventFinished() {
 }
 
 void setup() {
-  // Initialize serial communication at 9600 bits per second:
+  // Initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
+  // Initialize the DFPlayer Mini
+  FPSerial.begin(9600);
 
   pixels.begin(); // Initialize NeoPixels
   pixels.clear(); // Clear NeoPixels
 
-  // Set all defined input pins as INPUT with pull-up resistors
-  for (int i = 0; i < numberOfConnections; i++) {
-    pinMode(connectionsInputPins[i], INPUT_PULLUP);
+  uint8_t res = initDFPlayer();
+  if (res != 0) {
+    // If DFPlayer initialization failed, blink the NeoPixels red 3 times
+    for (int i = 0; i < 3; i++) {
+      showLEDSingleColor(pixels.Color(255, 0, 0), 100);
+      delay(100);
+    }
   }
 
-  // Set all defined output pins as OUTPUT
+  // Set all defined connections input pins to INPUT_PULLUP and output pins to OUTPUT
   for (int i = 0; i < numberOfConnections; i++) {
+    pinMode(connectionsInputPins[i], INPUT_PULLUP);
     pinMode(connectionsOutputPins[i], OUTPUT);
     digitalWrite(connectionsOutputPins[i], HIGH);
   }
+  delay(1000);
+  Serial.println("Setup complete. Starting connection checks...");
 }
 
 uint8_t getNumClosedConnections(uint8_t *states, int size) {
@@ -92,13 +179,11 @@ void loop() {
   unsigned long currentMillis = millis();
 
   // Check if it's time to read the pins
-  if (currentMillis - previousMillis >= interval) {
-    // Save the last time you checked the pins
+  if (currentMillis - previousMillis >= CHECK_CONNECTION_INTERVAL) {
     previousMillis = currentMillis;
 
     // Read the state of each pin and store it in the connectionStates array
     for (int i = 0; i < numberOfConnections; i++) {
-      //connectionStates[i] = digitalRead(connectionsInputPins[i]);
       connectionStates[i] = getConnectionState(i);
     }
 
@@ -106,7 +191,7 @@ void loop() {
     uint8_t numClosedConnections = getNumClosedConnections(connectionStates, numberOfConnections);
     bool allConnectionsClosed = false;
     if (numClosedConnections != previousNumClosedConnections) {
-      allConnectionsClosed = (numClosedConnections == numberOfConnections);
+      allConnectionsClosed = (numClosedConnections == 4);
     }
 
 
@@ -125,36 +210,24 @@ void loop() {
     }
 
     // Run LED events based on the connection state changes
-    if (connectionClosed) {
-      playConnectionClosedMelody(); // Play melody for connection closed
-      runLEDEventConnectionClosed(); // Run LED event for connection closed
-    }
-    if (connectionOpened) {
-      playConnectionOpenedMelody(); // Play melody for connection opened
-      runLEDEventConnectionOpened(); // Run LED event for connection opened
-    }
-
     if(allConnectionsClosed) {
-      playFinishedMelody(); // Play melody when all connections are closed
-      runLEDEventFinished(); // Run LED event when all connections are closed
+      Serial.println("All connections are closed.");
+      playFinishedMelody();
+      runLEDEventFinished();
+    }
+    else if (connectionClosed) {
+      Serial.println("A connection was closed.");
+      playConnectionClosedMelody();
+      runLEDEventConnectionClosed();
+    }
+    else if (connectionOpened) {
+      Serial.println("A connection was opened.");
+      playConnectionOpenedMelody();
+      runLEDEventConnectionOpened();
     }
 
-    // If a connection was opened or closed, output the state of all connections
-    if (connectionOpened || connectionClosed) {
-      Serial.println("Connection state changed:");
-    } else {
-      Serial.println("No change in connection state.");
-    }
 
-    // Output the array to serial
-    Serial.print("[");
-    for (int i = 0; i < numberOfConnections; i++) {
-      Serial.print(connectionStates[i]);
-      if (i < numberOfConnections - 1) {
-        Serial.print(", ");
-      }
-    }
-    Serial.println("]");
+    printConnectionStates();
   }
 }
 
@@ -258,16 +331,4 @@ void modeRandomFlash(unsigned long duration) {
     hueShift = (hueShift + 3) % 256;
     delay(frameInterval);
   }
-}
-
-void playFinishedMelody() {
-  
-}
-
-void playConnectionClosedMelody() {
-  
-}
-
-void playConnectionOpenedMelody() {
-  
 }
